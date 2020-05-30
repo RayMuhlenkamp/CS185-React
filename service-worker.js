367d4746d7cd82621f069bfd010ae67d604ddb6e
RayMuhlenkamp <rmuhlenkamp@umail.ucsb.edu>
2020-05-29 21:01:25 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-react/precache-manifest.6092728850d5e1b02f9c560f37c456b2.js"
+  "/cs185-react/precache-manifest.363699eb578016b6e24491bbcc208d1a.js"
 );
 
 self.addEventListener('message', (event) => {
