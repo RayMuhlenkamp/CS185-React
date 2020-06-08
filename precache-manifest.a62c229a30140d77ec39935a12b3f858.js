c7111d21f850800480d19b44e350767381c472ef
RayMuhlenkamp <rmuhlenkamp@umail.ucsb.edu>
2020-06-08 14:56:48 -0700

    Updates

---
+++ precache-manifest.a62c229a30140d77ec39935a12b3f858.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bf5f04687898c78e1cb7e016ba4bbb",
+    "url": "/cs185-react/index.html"
+  },
+  {
+    "revision": "c034dc52fe23fd8932f5",
+    "url": "/cs185-react/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "65290c93acdec0a87834",
+    "url": "/cs185-react/static/css/main.2e078b4d.chunk.css"
+  },
+  {
+    "revision": "c034dc52fe23fd8932f5",
+    "url": "/cs185-react/static/js/2.313a319c.chunk.js"
+  },
+  {
+    "revision": "454855a5c2283f0ec4fc47dedbba2838",
+    "url": "/cs185-react/static/js/2.313a319c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65290c93acdec0a87834",
+    "url": "/cs185-react/static/js/main.ed0b3328.chunk.js"
+  },
+  {
+    "revision": "4fb2594c8a0afa2fa1d8",
+    "url": "/cs185-react/static/js/runtime-main.f225555f.js"
+  },
+  {
+    "revision": "9ccd22356c2b2de21c6332d1fa9f875c",
+    "url": "/cs185-react/static/media/IMG_1328.9ccd2235.MOV"
+  },
+  {
+    "revision": "23045880864d64febc89f94d1ddb1476",
+    "url": "/cs185-react/static/media/catsper.23045880.mp4"
+  },
+  {
+    "revision": "113ddc205e0ec264a6115d166eb0b2dd",
+    "url": "/cs185-react/static/media/facekick.113ddc20.mp4"
+  },
+  {
+    "revision": "70f446f611e8741ffe89d2b71bce077d",
+    "url": "/cs185-react/static/media/im_champ1.70f446f6.jpg"
+  },
+  {
+    "revision": "857c802c7d6b2cc64a7467aff98f6f39",
+    "url": "/cs185-react/static/media/im_champ2.857c802c.jpg"
+  },
+  {
+    "revision": "9976372eda83c08b83153f1632d5c53e",
+    "url": "/cs185-react/static/media/im_champ3.9976372e.jpg"
+  },
+  {
+    "revision": "6470e251d0c2f75873a863d96bf3ffcb",
+    "url": "/cs185-react/static/media/iv_music1.6470e251.jpg"
+  },
+  {
+    "revision": "a809664aceb033bcd364176465133906",
+    "url": "/cs185-react/static/media/iv_music2.a809664a.jpg"
+  },
+  {
+    "revision": "3e694995a908f9878d21c8bdb9f88907",
+    "url": "/cs185-react/static/media/mom.3e694995.jpg"
+  },
+  {
+    "revision": "ae360914d413e27bbf2ff7079e85b3a3",
+    "url": "/cs185-react/static/media/polaroid.ae360914.jpg"
+  },
+  {
+    "revision": "f727a276de76a289866e11ba5895dad1",
+    "url": "/cs185-react/static/media/ray_linkedin.f727a276.jpg"
+  },
+  {
+    "revision": "e2daab4c92aacabd0b0326f2a03a0ff2",
+    "url": "/cs185-react/static/media/seattle1.e2daab4c.jpg"
+  },
+  {
+    "revision": "c7a5f3a1f8b0ee784ba0bbc71ebd3ced",
+    "url": "/cs185-react/static/media/seattle2.c7a5f3a1.jpg"
+  },
+  {
+    "revision": "ee0678fc0aba013c3cd316e509262096",
+    "url": "/cs185-react/static/media/seattle3.ee0678fc.jpg"
+  },
+  {
+    "revision": "c7a7a0e3a719fe2d9352d1ee3f21ffca",
+    "url": "/cs185-react/static/media/soccer.c7a7a0e3.jpg"
+  },
+  {
+    "revision": "57eb432bc25a0bf0b3a88be934f72b4c",
+    "url": "/cs185-react/static/media/spoider.57eb432b.mp4"
+  },
+  {
+    "revision": "27dae32a38ad201568e6e0dfd14efdaf",
+    "url": "/cs185-react/static/media/tshirt_black.27dae32a.png"
+  },
+  {
+    "revision": "aafe44bfe230fd43aa14b3d0e3112902",
+    "url": "/cs185-react/static/media/tshirt_white.aafe44bf.png"
+  },
+  {
+    "revision": "423b9712af893670c5be0fa5fef2b4a2",
+    "url": "/cs185-react/static/media/tshirt_yellow.423b9712.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-react/precache-manifest.363699eb578016b6e24491bbcc208d1a.js"
+  "/cs185-react/precache-manifest.a62c229a30140d77ec39935a12b3f858.js"
 );
 
 self.addEventListener('message', (event) => {
